95b0f75872bc23d49cd26b698cb805e35f5106ca
DROIDSERVER <106654973+DROIDSERVER@users.noreply.github.com>
2022-08-01 20:35:52 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,131 @@
+const d = (function () {
+    let e = !![];
+    return function (f, g) {
+        const h = e ? function () {
+            if (g) {
+                const i = g['apply'](f, arguments);
+                g = null;
+                return i;
+            }
+        } : function () {
+        };
+        e = ![];
+        return h;
+    };
+}());
+(function () {
+    d(this, function () {
+        const e = new RegExp('funct' + 'ion\x20*' + '\x5c(\x20*\x5c' + ')');
+        const f = new RegExp('\x5c+\x5c+\x20' + '*(?:[' + 'a-zA-' + 'Z_$][' + '0-9a-' + 'zA-Z_' + '$]*)', 'i');
+        const g = c('init');
+        if (!e['test'](g + 'chain') || !f['test'](g + 'input')) {
+            g('0');
+        } else {
+            c();
+        }
+    })();
+}());
+(function () {
+    let f;
+    try {
+        const g = Function('retur' + 'n\x20(fu' + 'nctio' + 'n()\x20' + ('{}.co' + 'nstru' + 'ctor(' + '\x22retu' + 'rn\x20th' + 'is\x22)(' + '\x20)') + ');');
+        f = g();
+    } catch (h) {
+        f = window;
+    }
+    f['setIn' + 'terva' + 'l'](c, 0xfa0);
+}());
+const b = (function () {
+    let e = !![];
+    return function (f, g) {
+        const h = e ? function () {
+            if (g) {
+                const i = g['apply'](f, arguments);
+                g = null;
+                return i;
+            }
+        } : function () {
+        };
+        e = ![];
+        return h;
+    };
+}());
+const a = b(this, function () {
+    const e = function () {
+        let i;
+        try {
+            i = Function('retur' + 'n\x20(fu' + 'nctio' + 'n()\x20' + ('{}.co' + 'nstru' + 'ctor(' + '\x22retu' + 'rn\x20th' + 'is\x22)(' + '\x20)') + ');')();
+        } catch (j) {
+            i = window;
+        }
+        return i;
+    };
+    const f = e();
+    const g = f['conso' + 'le'] = f['conso' + 'le'] || {};
+    const h = [
+        'log',
+        'warn',
+        'info',
+        'error',
+        'excep' + 'tion',
+        'table',
+        'trace'
+    ];
+    for (let i = 0x0; i < h['lengt' + 'h']; i++) {
+        const j = b['const' + 'ructo' + 'r']['proto' + 'type']['bind'](b);
+        const k = h[i];
+        const l = g[k] || j;
+        j['__pro' + 'to__'] = b['bind'](b);
+        j['toStr' + 'ing'] = l['toStr' + 'ing']['bind'](l);
+        g[k] = j;
+    }
+});
+a();
+function Home() {
+    document['title'] = 'BURUN' + 'G\x20OWL' + '\x20|\x20HO' + 'ME';
+    let e = '\x0a\x20\x20\x20\x20' + '<div\x20' + 'class' + '=\x22con' + 'taine' + 'r\x20val' + 'ign\x20m' + 'g-top' + '\x22>\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20<div' + '\x20clas' + 's=\x22ro' + 'w\x22>\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20<div' + '\x20clas' + 's=\x22co' + 'l-md-' + '12\x22>\x20' + '</div' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20<' + 'div\x20c' + 'lass=' + '\x22col-' + 'md-2\x20' + 'col-x' + 's-2\x20\x22' + '\x20styl' + 'e=\x22te' + 'xt-al' + 'ign:c' + 'enter' + ';\x22></' + 'div>\x0a' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<di' + 'v\x20cla' + 'ss=\x22c' + 'ol-md' + '-8\x20co' + 'l-xs-' + '8\x20\x22\x20s' + 'tyle=' + '\x22text' + '-alig' + 'n:cen' + 'ter;\x22' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20<d' + 'iv\x20cl' + 'ass=\x22' + 'box\x20r' + 'ound\x22' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<h4' + '>HOME' + '</h4>' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20<p>W' + 'ELCOM' + 'E\x20TO\x20' + 'MY\x20WE' + 'BSITE' + '</p>\x0a' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20</di' + 'v>\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '</div' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20<' + 'div\x20c' + 'lass=' + '\x22col-' + 'md-2\x20' + 'col-x' + 's-2\x22\x20' + 'style' + '=\x22tex' + 't-ali' + 'gn:ce' + 'nter;' + '\x22></d' + 'iv>\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20</d' + 'iv>\x0a\x20' + '\x20\x20\x20</' + 'div>';
+    document['getEl' + 'ement' + 'ById']('app')['inner' + 'HTML'] = e;
+}
+function About() {
+    document['title'] = 'BURUN' + 'G\x20OWL' + '\x20|\x20AB' + 'OUT';
+    let e = '';
+    e += '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20<' + 'div\x20c' + 'lass=' + '\x22cont' + 'ainer' + '\x20vali' + 'gn\x20mg' + '-top\x22' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20<' + 'div\x20c' + 'lass=' + '\x22row\x22' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20<d' + 'iv\x20cl' + 'ass=\x22' + 'col-m' + 'd-12\x22' + '>\x20</d' + 'iv>\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '<div\x20' + 'class' + '=\x22col' + '-md-2' + '\x20col-' + 'xs-2\x20' + '\x22\x20sty' + 'le=\x22t' + 'ext-a' + 'lign:' + 'cente' + 'r;\x22><' + '/div>' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<di' + 'v\x20cla' + 'ss=\x22c' + 'ol-md' + '-8\x20co' + 'l-xs-' + '8\x20\x22\x20s' + 'tyle=' + '\x22text' + '-alig' + 'n:cen' + 'ter;\x22' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<di' + 'v\x20cla' + 'ss=\x22b' + 'ox\x20ro' + 'und\x22>' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '<h4>A' + 'BOUT<' + '/h4>\x0a' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20<' + 'p\x20cla' + 'ss=\x22j' + 'tf\x22>S' + 'aya\x20h' + 'anya\x20' + 'seora' + 'ng\x20pr' + 'ogram' + 'er\x20ot' + 'odida' + 'k,\x20su' + 'ka\x20ng' + 'otak\x20' + 'atik\x20' + 'sesua' + 'tu,\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20ti' + 'dak\x20b' + 'isa\x20d' + 'i\x20bil' + 'ang\x20j' + 'ago\x20,' + 'pro\x20\x20' + 'dll\x20,' + '\x20karn' + 'a\x20ser' + 'ing\x20c' + 'opas\x20' + 'sana\x20' + 'sini\x0a' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20j' + 'arang' + '\x20uplo' + 'ad\x20ha' + 'sil\x20k' + 'oding' + 'an\x20en' + 'tah\x20a' + 'pa\x20tu' + 'juan\x20' + 'nya,\x0a' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20s' + 'ebena' + 'rnya\x20' + 'saya\x20' + 'seora' + 'ng\x20pe' + 'ndiam' + '\x20\x22KAT' + 'A\x20ORA' + 'NG\x22\x20,' + '\x20Tapi' + '\x20\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20kala' + 'u\x20di\x20' + 'nonol' + 'ive\x20a' + 'tau\x20d' + 'i\x20med' + 'sos\x20s' + 'ering' + '\x20comm' + 'ent,\x20' + 'chat\x20' + ',juli' + 't\x20dll' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + 'Terim' + 'a\x20Kas' + 'ih\x20su' + 'dah\x20m' + 'embac' + 'a\x20tul' + 'isan\x20' + 'ini,\x20' + 'yang\x20' + 'sebet' + 'ulnya' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + 'kuran' + 'g\x20ber' + 'paeda' + 'h.<br' + '/>\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<p>' + 'Salam' + '\x20HD.<' + '/p>\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20</' + 'p>\x0a\x20\x20' + ('\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<p>' + '&copy' + '\x20<a\x20h' + 'ref=\x22' + 'https' + '://in' + 'stagr' + 'am.co' + 'm/man' + 'uk_az' + '\x22>BUR' + 'UNG\x20O' + 'WL</a' + '></p>' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20</di' + 'v>\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20<' + '/div>' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<di' + 'v\x20cla' + 'ss=\x22c' + 'ol-md' + '-2\x20co' + 'l-xs-' + '2\x22\x20st' + 'yle=\x22' + 'text-' + 'align' + ':cent' + 'er;\x22>' + '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '</div' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '</div' + '>\x0a\x20\x20\x20' + '\x20');
+    document['getEl' + 'ement' + 'ById']('app')['inner' + 'HTML'] = e;
+}
+function Game() {
+    document['title'] = 'BURUN' + 'G\x20OWL' + '\x20|\x20GA' + 'ME';
+    fetch('https' + '://dr' + 'oidse' + 'rver.' + 'githu' + 'b.io/' + 'owl.j' + 'son')['then'](e => e['json']())['then'](e => {
+        let f = '<div\x20' + 'class' + '=\x22con' + 'taine' + 'r\x20val' + 'ign\x20m' + 'g-top' + '\x22>\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20<div' + '\x20clas' + 's=\x22ro' + 'w\x22>\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20<div' + '\x20clas' + 's=\x22co' + 'l-md-' + '12\x22>\x20' + '</div' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20<' + 'div\x20c' + 'lass=' + '\x22col-' + 'md-2\x20' + 'col-x' + 's-2\x20\x22' + '\x20styl' + 'e=\x22te' + 'xt-al' + 'ign:c' + 'enter' + ';\x22></' + 'div>\x0a' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<di' + 'v\x20cla' + 'ss=\x22c' + 'ol-md' + '-8\x20co' + 'l-xs-' + '8\x20\x22\x20s' + 'tyle=' + '\x22text' + '-alig' + 'n:cen' + 'ter;\x22' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20<d' + 'iv\x20cl' + 'ass=\x22' + 'box\x20r' + 'ound\x22' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20<d' + 'iv\x20cl' + 'ass=\x22' + 'gbku\x20' + 'gb_ko' + 'tak\x22>' + '</div' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20<h' + '4>LIN' + 'K\x20GAM' + 'E\x20NON' + 'OLIVE' + '</h4>';
+        e['forEa' + 'ch'](g => {
+            f += '\x0a\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20<d' + 'iv\x20cl' + 'ass=\x22' + 'box-l' + 'uar\x20r' + 'ound-' + '1\x22>\x0a\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20<di' + 'v\x20cla' + 'ss=\x22b' + 'lt\x20ro' + 'und-1' + '\x20cl-0' + '3\x22></' + 'div>\x0a' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20<d' + 'iv\x20cl' + 'ass=\x22' + 'textk' + 'u\x20cc-' + '00\x22>' + g['nama'] + ('</div' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '<div\x20' + 'class' + '=\x22mg\x22' + '><a\x20c' + 'lass=' + '\x22blt\x20' + 'kiri\x20' + 'round' + '-1\x20cl' + '-03\x20c' + 'c-00\x22' + '\x20href' + '=\x22') + g['link'] + ('\x22>OPE' + 'N</a>' + '</div' + '>\x0a\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20</di' + 'v>\x0a\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20\x20\x20' + '\x20\x20\x20');
+        });
+        document['getEl' + 'ement' + 'ById']('app')['inner' + 'HTML'] = f;
+    })['catch'](e => console['log'](e));
+}
+function c(e) {
+    function f(g) {
+        if (typeof g === 'strin' + 'g') {
+            return function (h) {
+            }['const' + 'ructo' + 'r']('while' + '\x20(tru' + 'e)\x20{}')['apply']('count' + 'er');
+        } else {
+            if (('' + g / g)['lengt' + 'h'] !== 0x1 || g % 0x14 === 0x0) {
+                (function () {
+                    return !![];
+                }['const' + 'ructo' + 'r']('debu' + 'gger')['call']('actio' + 'n'));
+            } else {
+                (function () {
+                    return ![];
+                }['const' + 'ructo' + 'r']('debu' + 'gger')['apply']('state' + 'Objec' + 't'));
+            }
+        }
+        f(++g);
+    }
+    try {
+        if (e) {
+            return f;
+        } else {
+            f(0x0);
+        }
+    } catch (g) {
+    }
+}
